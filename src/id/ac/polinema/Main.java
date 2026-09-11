faa7e503df13540e4e14e452dafd0de033d8ce15
Vallent12 <fevadtya09.gmail.com>
2026-09-12 00:00:06 +0700

    FINAL

--- src/id/ac/polinema/Main.java
+++ src/id/ac/polinema/Main.java
@@ -2,8 +2,12 @@ package id.ac.polinema;
 
 public class Main {
     public static void main(String[] args) {
-        Account acc = new Account("Nadia", 500000);
-        acc.withdraw(150000);
-        acc.printInfo();
+        Account from = new Account("Nadia", 500000);
+        Account to = new Account("Budi", 200000);
+
+        from.transferTo(to, 100000);
+
+        from.printInfo();
+        to.printInfo();
     }
 }
