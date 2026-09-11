91ebd27b181f303d7a1ba56859f4a575621b9555
Vallent12 <fevadtya09.gmail.com>
2026-09-11 08:42:43 +0700

    Menambahkan Method

--- src/id/ac/polinema/Account.java
+++ src/id/ac/polinema/Account.java
@@ -3,4 +3,16 @@ package id.ac.polinema;
 public class Account {
     public String ownerName;
     public double balance;
+
+    public void deposit(double amount) {
+        balance = balance + amount;
+    }
+
+    public void withdraw(double amount){
+        balance = balance - amount;
+    }
+
+    public void printInfo() {
+        System.out.println(ownerName + " - balance: " + balance);
+    }
 }
