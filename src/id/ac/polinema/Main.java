46b0e28b835d6692b536547dde12351c647dc7c7
Vallent12 <fevadtya09.gmail.com>
2026-09-18 05:14:25 +0700

    memakai constructor accout baru

--- src/id/ac/polinema/Main.java
+++ src/id/ac/polinema/Main.java
@@ -2,12 +2,8 @@ package id.ac.polinema;
 
 public class Main {
     public static void main(String[] args) {
-        Account from = new Account("Nadia", 500000);
-        Account to = new Account("Budi", 200000);
-
-        from.transferTo(to, 100000);
-
-        from.printInfo();
-        to.printInfo();
+       Account acc = new Account("A001", "Nadia", 500000);
+       acc.withdraw(150000);
+       acc.printInfo();
     }
 }
