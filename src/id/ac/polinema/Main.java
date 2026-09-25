cb6dca8cadfb16629664003ed041d00f10c40c12
Vallent12 <fevadtya09.gmail.com>
2026-09-25 17:35:39 +0700

    memakai Bank untuk mengelola dua Account

--- src/id/ac/polinema/Main.java
+++ src/id/ac/polinema/Main.java
@@ -3,8 +3,19 @@ package id.ac.polinema;
 public class Main {
     public static void main(String[] args) {
         Customer customer1 = new Customer("nadia", "0812-0000-0001");
-        Account aacl = new Account("A001", customer1, 500000 );
-        aacl.withdraw(150000);
-        aacl.printInfo();
+        Account accl = new Account("A001", customer1, 500000 );
+        accl.withdraw(150000);
+
+        Customer customer2 = new Customer("sari", "0812-0000-0002");
+        Account acc2 = new Account("A002", customer2, 200000 );
+
+        Bank bank = new Bank(10);
+        bank.addAccount(accl);
+        bank.addAccount(acc2);
+        bank.printAllAccounts();
+        Account found = bank.findAccount("A002");
+        if (found != null) {
+            found.printInfo();
+        }
     }
 }
