4672faa98fd87d9869c1b078850d72b69157200c
Vallent12 <fevadtya09.gmail.com>
2026-09-25 17:54:50 +0700

    tugas mandiri

--- src/id/ac/polinema/Main.java
+++ src/id/ac/polinema/Main.java
@@ -3,19 +3,26 @@ package id.ac.polinema;
 public class Main {
     public static void main(String[] args) {
         Customer customer1 = new Customer("nadia", "0812-0000-0001");
-        Account accl = new Account("A001", customer1, 500000 );
-        accl.withdraw(150000);
-
         Customer customer2 = new Customer("sari", "0812-0000-0002");
-        Account acc2 = new Account("A002", customer2, 200000 );
+
+        Account acc1 = new Account("A001", customer1, 350000);
+        Account acc2 = new Account("A002", customer2, 200000);
+        Account acc3 = new Account("A003", customer1, 1000000);
 
         Bank bank = new Bank(10);
-        bank.addAccount(accl);
+        bank.addAccount(acc1);
         bank.addAccount(acc2);
+        bank.addAccount(acc3);
+
+        System.out.println("--- Daftar Seluruh Rekening ---");
         bank.printAllAccounts();
-        Account found = bank.findAccount("A002");
-        if (found != null) {
-            found.printInfo();
+
+       System.out.println("\n--- Hasil Pencarian Rekening Nadia ---");
+        Account[] nadiaAccounts = bank.findAccountsByOwnerName("nadia");
+        System.out.println("Jumlah rekening ditemukan: " + nadiaAccounts.length);
+        for (Account acc : nadiaAccounts) {
+            acc.printInfo();
+
         }
     }
 }
